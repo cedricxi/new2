a48f39348900e5ba61e75c8c3236b9b8797f5a07
cedricxi <bikovdanilaser@gmail.com>
2025-06-26 20:50:18 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
 
 //TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
@@ -6,44 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         while(true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите число 1");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("введите число 1");
+            float n = sc.nextInt();
+            System.out.println("введите число 2");
+            float m = sc.nextInt();
+            System.out.println("введите число 3");
+            float f = sc.nextInt();
 
-            float n = scanner.nextInt();
-            System.out.println("Введите число 2");
-
-            float m = scanner.nextInt();
-            System.out.println("Введите операцию");
-            String b = scanner.next();
-            System.out.println("Введите операцию");
-
-
-                switch (b) {
-                    case "+" -> System.out.println(n + m);
-                    case "-" -> System.out.println(n - m);
-                    case "*" -> System.out.println(n * m);
-                    case "/" -> System.out.println(n / m);
-                    default -> System.out.println("Операция не распознана. Повторите ввод.");
-
-                }
-
+            if (n > m && n > f) {
+                System.out.println(n);
+            } else if  (n < f && f > m) {
+                System.out.println(f);
+            }else{
+                System.out.println(m);
+            }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
 }
