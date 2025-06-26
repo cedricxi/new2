10ca393c1ead6e2012b715b76e0309795fa9c2b4
cedricxi <bikovdanilaser@gmail.com>
2025-06-26 20:51:45 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
+// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
 public class Main {
     public static void main(String[] args) {
         while(true) {
