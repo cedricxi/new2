c9fa0fb5cd5fffb6efc3b8854d9b44786f79d326
cedricxi <bikovdanilaser@gmail.com>
2025-06-26 20:51:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
-// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
 public class Main {
     public static void main(String[] args) {
         while(true) {
